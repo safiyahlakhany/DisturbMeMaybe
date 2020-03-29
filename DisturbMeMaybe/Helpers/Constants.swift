e06428ad1917bea61d35d2cb99fb3dbe9406faad
saadiak <saadiak@uci.edu>
2020-03-29 03:25:33 -0700

    Changed twilio stuff

--- DisturbMeMaybe/Helpers/Constants.swift
+++ DisturbMeMaybe/Helpers/Constants.swift
@@ -13,10 +13,10 @@ struct Constants {
     
     struct TwilioStuff {
         
-        static let fromNumber = "+12512505196"
-        static let url = "https://api.twilio.com/2010-04-01/Accounts/AC4c595153e081de4631644b9c7bb00276/Messages"
-        static let username = "AC4c595153e081de4631644b9c7bb00276"
-        static let password = "c75cbdc133506431223b7282af32aebb"
+        static let fromNumber = "+15718319133"
+        static let url = "https://api.twilio.com/2010-04-01/Accounts/AC5fa004e52e189a679f1be70f5534d9c3/Messages"
+        static let username = "AC5fa004e52e189a679f1be70f5534d9c3"
+        static let password = "be12d9551a4db93caa7e6ceea1e51b61"
         
     }
     
